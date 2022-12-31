b04b6e620e8f16ef3bb2748659595b6994152e65
akutz <sakutz@gmail.com>
2022-12-30 19:59:19 -0600

    Root imports
    This patch adds the files at the root of the project which import
    the revelant packages based on the Go version.

---
+++ import-1.18.go
@@ -0,0 +1,53 @@
+//go:build go1.18 && !go1.19
+
+// Copyright 2022 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package json
+
+import (
+	"io"
+
+	"github.com/akutz/gdj/go1/1.18/1.18.9"
+)
+
+// DiscriminatorEncodeMode is a mask that describes the different encode
+// options.
+type DiscriminatorEncodeMode = json.DiscriminatorEncodeMode
+
+const (
+	// DiscriminatorEncodeTypeNameIfRequired is the default behavior when
+	// the discriminator is set, and the type name is only encoded if required.
+	DiscriminatorEncodeTypeNameIfRequired = json.DiscriminatorEncodeTypeNameIfRequired
+
+	// DiscriminatorEncodeTypeNameRootValue causes the type name to be encoded
+	// for the root value.
+	DiscriminatorEncodeTypeNameRootValue = json.DiscriminatorEncodeTypeNameRootValue
+
+	// DiscriminatorEncodeTypeNameAllObjects causes the type name to be encoded
+	// for all struct and map values. Please note this specifically does not
+	// apply to the root value.
+	DiscriminatorEncodeTypeNameAllObjects = json.DiscriminatorEncodeTypeNameAllObjects
+
+	// DiscriminatorEncodeTypeNameWithPath causes the type name to be encoded
+	// prefixed with the type's full package path.
+	DiscriminatorEncodeTypeNameWithPath = json.DiscriminatorEncodeTypeNameWithPath
+)
+
+// DiscriminatorToTypeFunc is used to get a reflect.Type from its
+// discriminator.
+type DiscriminatorToTypeFunc = json.DiscriminatorToTypeFunc
+
+// NewEncoder returns a new encoder that writes to w.
+func NewEncoder(w io.Writer) *json.Encoder {
+	return json.NewEncoder(w)
+}
+
+// NewDecoder returns a new decoder that reads from r.
+//
+// The decoder introduces its own buffering and may
+// read data from r beyond the JSON values requested.
+func NewDecoder(r io.Reader) *json.Decoder {
+	return json.NewDecoder(r)
+}
